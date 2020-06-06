645ab0f151d7c4089760e86f4e8f1ee2e0be589d
Karsten Koehler <karsten.koehler95@gmail.com>
2020-06-06 17:54:17 +0200

    join all geoms in segmentation table

--- scripts/shp_to_geom.py
+++ scripts/shp_to_geom.py
@@ -6,64 +6,80 @@ import psycopg2
 def import_shapefiles(files):
     # create intermediate shape tables
     for f in files:
-        cmd = f"shp2pgsql -s 25832 -g geom -e -S -t 2D -N skip {os.path.join(base_path, f)}.shp public.geom_segments_{f} | psql -d dop10rgbi_nrw -q"
+        cmd = f"shp2pgsql -s 25832 -g geom -e -S -t 2D -N skip {os.path.join(base_path, f)}.shp public.geom_tmp_{f} | psql -d dop10rgbi_nrw -q"
         print(cmd)
         subprocess.run(cmd, shell=True, check=True)
 
 
-def create_segmentation_table():
-    # create final segmentation table
-    cur = db.cursor()
-    stmt = """CREATE TABLE public.geom_segments (
-      gid SERIAL PRIMARY KEY, 
-      object_type INTEGER, 
-      object_description TEXT, 
-      segmentation_category INTEGER, 
-      geom geometry);"""
-    cur.execute(stmt)
-    cur.close()
+def create_tmp_segmentation_table():
+    # create temporary segmentation table
+    with db.cursor() as cur:
+        stmt = """CREATE TABLE public.geom_tmp (
+          gid SERIAL PRIMARY KEY, 
+          object_type INTEGER, 
+          object_description TEXT, 
+          segmentation_category INTEGER, 
+          geom geometry);"""
+        cur.execute(stmt)
 
 
 def filter_and_cut_shapes(files):
     # filter and cut all geoms, import to segmentation table
-    base_statement = """INSERT INTO public.geom_segments (object_type, object_description, geom)
-        (SELECT geom_segments_{0}.objart::int, geom_segments_{0}.objart_txt, ST_Intersection(geom_segments_{0}.geom, geom_bounds.geom)
-        FROM geom_segments_{0}, geom_bounds
-        WHERE ST_Intersects(geom_segments_{0}.geom, geom_bounds.geom));
+    base_statement = """INSERT INTO public.geom_tmp (object_type, object_description, geom)
+        (SELECT geom_tmp_{0}.objart::int, geom_tmp_{0}.objart_txt, ST_Intersection(geom_tmp_{0}.geom, geom_bounds.geom)
+        FROM geom_tmp_{0}, geom_bounds
+        WHERE ST_Intersects(geom_tmp_{0}.geom, geom_bounds.geom));
     """
-    cur = db.cursor()
-    for f in files:
-        stmt = base_statement.format(f)
-        print(stmt)
+    with db.cursor() as cur:
+        for f in files:
+            stmt = base_statement.format(f)
+            print(stmt)
+            cur.execute(stmt)
+
+
+def create_final_segmentation_table():
+    # create final segmentation table
+    with db.cursor() as cur:
+        stmt = """CREATE TABLE public.geom_segments (
+          gid SERIAL PRIMARY KEY, 
+          segmentation_description TEXT,
+          geom geometry);"""
         cur.execute(stmt)
-    cur.close()
 
 
-def map_to_segments():
+def map_and_merge_segments():
     # map object type to segmentation category
     object_type_mapping = {
-        # 0: water, 1: buildings, 2: agriculture, 3: forest, 4: urban greens, 5: traffic
-        44001: 0, 44005: 0, 44006: 0, 41007: 1, 41009: 4, 41002: 1,
-        41010: 1, 41008: 4, 41005: 1, 43001: 2, 43002: 3, 43003: 3,
-        43004: 3, 43007: 2, 42009: 5, 42001: 5, 42010: 5
+        "water": [44001, 44005, 44006],
+        "buildings": [41002, 41005, 41007, 41010],
+        "agriculture": [43001, 43007],
+        "forest": [43002, 43003, 43004],
+        "urban greens": [41008, 41009],
+        "traffic": [42001, 42009, 42010],
     }
 
-    cur = db.cursor()
-    for object_type, object_segment in object_type_mapping.items():
-        stmt = f"UPDATE public.geom_segments SET segmentation_category={object_segment} WHERE object_type={object_type}"
-        print(stmt)
-        cur.execute(stmt)
-    cur.close()
+    with db.cursor() as cur:
+        for cat, ids in object_type_mapping.items():
+            ids = [str(x) for x in ids]
+            stmt = """INSERT INTO geom_segments (segmentation_description, geom) 
+                SELECT '{0}' segmentation_category, ST_Union(geom) 
+                FROM geom_tmp
+                WHERE object_type IN ({1});""".format(cat, ','.join(ids))
+            print(stmt)
+            cur.execute(stmt)
 
 
 def clean_up_database(files):
     # remove intermediate shape tables
-    cur = db.cursor()
-    for f in files:
-        stmt = f"DROP TABLE public.geom_segments_{f};"
+    with db.cursor() as cur:
+        for f in files:
+            stmt = f"DROP TABLE public.geom_tmp_{f};"
+            print(stmt)
+            cur.execute(stmt)
+
+        stmt = "DROP TABLE public.geom_tmp"
         print(stmt)
         cur.execute(stmt)
-    cur.close()
 
 
 if __name__ == '__main__':
@@ -74,9 +90,10 @@ if __name__ == '__main__':
     db.autocommit = True
 
     import_shapefiles(files)
-    create_segmentation_table()
+    create_tmp_segmentation_table()
     filter_and_cut_shapes(files)
-    map_to_segments()
+    create_final_segmentation_table()
+    map_and_merge_segments()
     clean_up_database(files)
 
     db.close()
