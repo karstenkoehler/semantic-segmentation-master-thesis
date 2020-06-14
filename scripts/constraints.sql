90cd46c3d417eabde14a90c07c4632ff4e626b36
Karsten Koehler <karsten.koehler95@gmail.com>
2020-06-14 17:01:21 +0200

    delete sql scripts

--- scripts/constraints.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX ON public.dop_rgb USING gist (st_convexhull(rast));
-CREATE INDEX ON public.dop_nir USING gist (st_convexhull(rast));
-
-SELECT AddRasterConstraints('public','dop_rgb','rast',TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE);
-SELECT AddRasterConstraints('public','dop_nir','rast',TRUE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE);
--- scripts/create.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE public.dop_rgb (rid serial PRIMARY KEY, rast raster, filename text);
-CREATE TABLE public.dop_nir (rid serial PRIMARY KEY, rast raster, filename text);
