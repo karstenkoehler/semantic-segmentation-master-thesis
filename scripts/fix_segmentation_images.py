6301aef792e00823bf98cc7e574ff806c27086ab
Karsten Koehler <karsten.koehler95@gmail.com>
2020-06-16 10:50:33 +0200

    fix unet labels

--- scripts/fix_segmentation_images.py
+++ scripts/fix_segmentation_images.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.spatial as sp
 import time
 
-base_dir = os.path.join("E:", "data", "segmentation_labels_fixed")
+base_dir = os.path.join("E:", "data", "unet", "labels")
 valid_colors = [
     (240, 126, 11),  # water
     (3, 0, 208),  # buildings
@@ -47,7 +47,7 @@ def fix_image_pixels(image):
     return image
 
 
-def fix_and_save_image(file_name, base_dir=os.path.join("E:", "data", "segmentation_labels_fixed")):
+def fix_and_save_image(file_name):
     file_path = os.path.join(base_dir, file_name)
     image = cv2.imread(file_path)
     image = fix_image_pixels(image)
@@ -59,7 +59,6 @@ if __name__ == '__main__':
 
     for _, _, files in os.walk(base_dir):
         print(f"{len(files)} files to check")
-        invalid_pixel_count = 0
         invalid_files = set()
 
         count = 0
@@ -67,13 +66,12 @@ if __name__ == '__main__':
             count += 1
             image = cv2.imread(os.path.join(base_dir, f))
             if not validate_image(image):
-                invalid_pixel_count += 1
                 invalid_files.add(f)
 
             if count % 1000 == 0:
                 print(f"{count}/{len(files)} - ({time.time() - start:.2f})")
 
-        print(f"found {invalid_pixel_count} invalid pixels in {len(invalid_files)} files:")
+        print(f"found {len(invalid_files)} invalid files:")
         for file_name in invalid_files:
             fix_and_save_image(file_name)
             print(f"fixed {file_name}")
