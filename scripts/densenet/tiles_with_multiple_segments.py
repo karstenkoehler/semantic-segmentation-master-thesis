04fd36ab6e4ee34bb2c530523452b472ec389e1b
Karsten Koehler <karsten.koehler95@gmail.com>
2020-07-04 12:59:47 +0200

    fix multi-segments detection script

--- scripts/densenet/tiles_with_multiple_segments.py
+++ scripts/densenet/tiles_with_multiple_segments.py
@@ -4,7 +4,7 @@ db = psycopg2.connect("dbname='dop10rgbi_nrw' user='postgres' host='localhost' p
 
 with db.cursor() as cur:
     stmt = """SELECT geom_tiles_densenet.gid FROM geom_tiles_densenet, geom_segments
-        WHERE ST_Intersects(geom_tiles_densenet.geom_label, geom_segments.geom)
+        WHERE NOT geom_tiles_densenet.test_set AND ST_Intersects(geom_tiles_densenet.geom_label, geom_segments.geom)
         GROUP BY geom_tiles_densenet.gid HAVING COUNT(geom_tiles_densenet.gid) > 1;"""
     cur.execute(stmt)
     gids = [str(gid[0]) for gid in cur.fetchall()]
