521849b0d97eec86b28b17803ac6f533b475df39
Karsten Koehler <karsten.koehler95@gmail.com>
2020-07-14 09:17:40 +0200

    fix script imports

--- scripts/densenet/create_exportable_tiles.py
+++ scripts/densenet/create_exportable_tiles.py
@@ -1,4 +1,4 @@
-from ..common.create_exportable_tiles import create_exportable_tile_table
+from scripts.common.create_exportable_tiles import create_exportable_tile_table
 
 if __name__ == '__main__':
     create_exportable_tile_table(table_suffix="densenet", tile_size=256.0, label_size=256.0)
--- scripts/unet/create_exportable_tiles.py
+++ scripts/unet/create_exportable_tiles.py
@@ -1,4 +1,4 @@
-from ..common.create_exportable_tiles import create_exportable_tile_table
+from scripts.common.create_exportable_tiles import create_exportable_tile_table
 
 if __name__ == '__main__':
     create_exportable_tile_table(table_suffix="unet", tile_size=57.2, label_size=38.8)
