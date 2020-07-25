214178a0c940550c94836a014c1912b54012b0e6
Karsten Koehler <karsten.koehler95@gmail.com>
2020-07-25 18:45:22 +0200

    unet: define model in separate file

--- models/unet/model.py
+++ models/unet/model.py
@@ -5,68 +5,21 @@ import glob
 
 from datetime import datetime
 
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, Cropping2D, concatenate
 from tensorflow.keras.metrics import Accuracy, CategoricalAccuracy, MeanIoU
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, CSVLogger
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import load_model
 from tensorflow.python.keras.backend import set_value
 
 import numpy as np
 
 from models.common.common import get_training_gids_from_database, one_hot_to_rgb
 from models.common.data_generator import initialize_train_and_validation_generators
+from models.unet.unet import UNet
 
 
 def define_and_compile_model(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=None):
-    # contracting path
-    input_size = (572, 572, 3)
-    input = Input(input_size)
-    conv1 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(input)
-    conv1 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    conv5 = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-
-    # expansive path
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(drop5))
-    merge6 = concatenate([Cropping2D(4)(drop4), up6], axis=3)
-    conv6 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv6))
-    merge7 = concatenate([Cropping2D(16)(conv3), up7], axis=3)
-    conv7 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv7)
-
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv7))
-    merge8 = concatenate([Cropping2D(40)(conv2), up8], axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv8)
-
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv8))
-    merge9 = concatenate([Cropping2D(88)(conv1), up9], axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv9)
-    conv10 = Conv2D(6, 1, activation='sigmoid')(conv9)
-
-    model = Model(inputs=input, outputs=conv10)
+    model = UNet()
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
     return model
 
---
+++ models/unet/unet.py
@@ -0,0 +1,46 @@
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, Cropping2D, concatenate
+from tensorflow.keras.models import Model
+
+
+def UNet(input_size=(572, 572, 3)):
+    x = Input(input_size)
+
+    x, skip1 = _downsampling_block(x, 64)
+    x, skip2 = _downsampling_block(x, 128)
+    x, skip3 = _downsampling_block(x, 256)
+    x, skip4 = _downsampling_block(x, 512)
+
+    x = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    x = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    x = Dropout(0.5)(x)
+
+    x = _upsampling_block(x, skip4, 512, 4)
+    x = _upsampling_block(x, skip3, 256, 16)
+    x = _upsampling_block(x, skip2, 128, 40)
+    x = _upsampling_block(x, skip1, 64, 88)
+
+    out = Conv2D(6, 1, activation='sigmoid')(x)
+    return Model(inputs=input, outputs=out)
+
+
+def _downsampling_block(x, nb_filters):
+    x = Conv2D(nb_filters, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    skip = Conv2D(nb_filters, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(skip)
+    return x, skip
+
+
+def _upsampling_block(x, skip, nb_filters, crop_px):
+    skip = Cropping2D(crop_px)(skip)
+
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(nb_filters, 2, activation='relu', padding='same', kernel_initializer='he_normal')(x)
+    x = concatenate([skip, x], axis=3)
+    x = Conv2D(nb_filters, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    x = Conv2D(nb_filters, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(x)
+    return x
+
+
+if __name__ == '__main__':
+    model = UNet()
+    model.summary()
