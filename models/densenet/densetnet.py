cc4c23ce6dbe7a6b29bedc3c5b64a0362760ac51
Karsten Koehler <karsten.koehler95@gmail.com>
2020-07-29 18:37:33 +0200

    densenet: fixed l2 regularization

--- models/densenet/densetnet.py
+++ models/densenet/densetnet.py
@@ -4,52 +4,47 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.regularizers import l2
 
 
-def DenseNet():
-    input_size = (256, 256, 3)
-    growth_rate = 16
-    weight_decay = 1e-4
-    dense_block_layers = [4, 5, 7, 10, 12, 15]
-    dropout = 0.2
-    compression = 1.0
-    bottleneck = True
-    nb_filters = 48
+def DenseNet(dense_block_layers=None, growth_rate=16, initial_nb_filters=48, compression=1.0, bottleneck=True,
+             dropout=0.2, model_name_suffix="", input_size=(256, 256, 3)):
+    nb_filters = initial_nb_filters
+    nb_conv_layers = 0
+    if dense_block_layers is None:
+        dense_block_layers = [4, 5, 7, 10, 12, 15]
     skip_connections = []
 
     input_layer = Input(input_size)
-    x = Conv2D(nb_filters, (3, 3), padding="same", use_bias=False, kernel_regularizer=l2(weight_decay))(input_layer)
+    x = Conv2D(nb_filters, (3, 3), padding="same", use_bias=False, kernel_regularizer=_l2reg())(input_layer)
 
     for i, block_size in enumerate(dense_block_layers):
-        x, nb_filters = _dense_block(x, block_size, nb_filters, growth_rate, dropout, bottleneck, weight_decay)
+        x, nb_filters = _dense_block(x, block_size, nb_filters, growth_rate, dropout, bottleneck)
 
         skip_connections.append(x)
         if i < len(dense_block_layers) - 1:
-            x, nb_filters = _transition_down_layer(x, nb_filters, dropout, compression, weight_decay)
+            x, nb_filters = _transition_down_layer(x, nb_filters, dropout, compression)
 
     skip_connections = skip_connections[::-1][1:]
 
     for i, block_size in enumerate(dense_block_layers[::-1][1:]):
         x, nb_filters = _transition_up_layer(skip_connections[i], x, nb_filters)
+        x, nb_filters = _dense_block(x, block_size, nb_filters, growth_rate, dropout, bottleneck)
 
-        x, nb_filters = _dense_block(x, block_size, nb_filters, growth_rate, dropout, bottleneck, weight_decay)
-
-    x = Conv2D(6, (1, 1), activation="softmax", kernel_regularizer=l2(weight_decay), bias_regularizer=l2(weight_decay))(
-        x)
+    x = Conv2D(6, (1, 1), activation="softmax", kernel_regularizer=_l2reg(), bias_regularizer=_l2reg())(x)
 
     return Model(inputs=input_layer, outputs=x)
 
 
-def _convolution_block(x, nb_filters, dropout_rate=None, bottleneck=False, weight_decay=1e-4):
+def _convolution_block(x, nb_filters, dropout_rate=None, bottleneck=False):
     if bottleneck:
-        x = BatchNormalization(gamma_regularizer=l2(weight_decay), beta_regularizer=l2(weight_decay))(x)
+        x = BatchNormalization(gamma_regularizer=_l2reg(), beta_regularizer=_l2reg())(x)
         x = Activation("relu")(x)
-        x = Conv2D(nb_filters * 4, (1, 1), padding="same", use_bias=False, kernel_regularizer=l2(weight_decay))(x)
+        x = Conv2D(nb_filters * 4, (1, 1), padding="same", use_bias=False, kernel_regularizer=_l2reg())(x)
 
         if dropout_rate:
             x = Dropout(dropout_rate)(x)
 
-    x = BatchNormalization(gamma_regularizer=l2(weight_decay), beta_regularizer=l2(weight_decay))(x)
+    x = BatchNormalization(gamma_regularizer=_l2reg(), beta_regularizer=_l2reg())(x)
     x = Activation("relu")(x)
-    x = Conv2D(nb_filters, (3, 3), padding="same", use_bias=False, kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(nb_filters, (3, 3), padding="same", use_bias=False, kernel_regularizer=_l2reg())(x)
 
     if dropout_rate:
         x = Dropout(dropout_rate)(x)
@@ -57,20 +52,20 @@ def _convolution_block(x, nb_filters, dropout_rate=None, bottleneck=False, weigh
     return x
 
 
-def _dense_block(x, nb_layers, nb_filters, growth_rate, dropout_rate=None, bottleneck=False, weight_decay=1e-4):
+def _dense_block(x, nb_layers, nb_filters, growth_rate, dropout_rate=None, bottleneck=False):
     for _ in range(nb_layers):
-        block = _convolution_block(x, growth_rate, dropout_rate, bottleneck, weight_decay)
+        block = _convolution_block(x, growth_rate, dropout_rate, bottleneck)
         x = Concatenate(axis=3)([x, block])
         nb_filters += growth_rate
 
     return x, nb_filters
 
 
-def _transition_down_layer(x, nb_filters, dropout_rate=None, compression=1.0, weight_decay=1e-4):
+def _transition_down_layer(x, nb_filters, dropout_rate=None, compression=1.0):
     nb_filters = int(nb_filters * compression)
-    x = BatchNormalization(gamma_regularizer=l2(weight_decay), beta_regularizer=l2(weight_decay))(x)
+    x = BatchNormalization(gamma_regularizer=_l2reg(), beta_regularizer=_l2reg())(x)
     x = Activation("relu")(x)
-    x = Conv2D(nb_filters, (1, 1), padding="same", use_bias=False, kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(nb_filters, (1, 1), padding="same", use_bias=False, kernel_regularizer=_l2reg())(x)
 
     if dropout_rate:
         x = Dropout(dropout_rate)(x)
@@ -83,3 +78,7 @@ def _transition_up_layer(skip_connection, x, nb_filters):
     x = Conv2DTranspose(nb_filters, (2, 2), strides=(2, 2))(x)
     x = Concatenate(axis=3)([x, skip_connection])
     return x, nb_filters
+
+
+def _l2reg(weight_decay=1e-4):
+    return l2(weight_decay)
