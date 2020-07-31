593fca97a86e3bc39ed54992c31a5cd70a7c5869
Karsten Köhler <karsten.koehler95@gmail..com>
2020-07-31 11:31:48 +0200

    wnet: update training method

--- models/wnet/model.py
+++ models/wnet/model.py
@@ -1,112 +1,20 @@
 import os
-import glob
 import os
-import random
 import time
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate
 from tensorflow.keras.metrics import Accuracy, CategoricalAccuracy
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.python.keras.backend import set_value
+from tensorflow.keras.models import load_model
+from tensorflow.python.keras.callbacks import CSVLogger, ReduceLROnPlateau
+from tensorflow.python.keras.losses import MSE
+from tensorflow.python.keras.metrics import CategoricalCrossentropy
 
 from models.common.callbacks import metrics_to_csv_logger, save_model_on_epoch_end
 from models.common.common import get_gids_from_database, split_to_tiles
-
-
-def define_and_compile_model(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=None):
-    # contracting path
-    input_size = (224, 224, 3)
-    input = Input(input_size)
-    classification = unet(input, num_classes=1000)
-    reconstructed = unet(classification, num_classes=3, output_activation="sigmoid")
-
-    model = Model(inputs=input, outputs=reconstructed)
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-    encoder = Model(inputs=input, outputs=classification)
-    encoder.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-    return model, encoder
-
-
-def unet(input, num_classes, output_activation="softmax"):
-    conv1 = Conv2D(64, 3, activation='relu', padding="same", kernel_initializer='he_normal')(input)
-    conv1 = Conv2D(64, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation='relu', padding="same", kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation='relu', padding="same", kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation='relu', padding="same", kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding="same", kernel_initializer='he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-    # expansive path
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(drop5))
-    merge6 = concatenate([drop4, up6], axis=3)
-    conv6 = Conv2D(512, 3, activation='relu', padding="same", kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv6)
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv6))
-    merge7 = concatenate([conv3, up7], axis=3)
-    conv7 = Conv2D(256, 3, activation='relu', padding="same", kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv7)
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv7))
-    merge8 = concatenate([conv2, up8], axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding="same", kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv8)
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv8))
-    merge9 = concatenate([conv1, up9], axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding="same", kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding="same", kernel_initializer='he_normal')(conv9)
-    conv10 = Conv2D(num_classes, 1, activation=output_activation)(conv9)
-    return conv10
-
-
-def data_generator(gids, batch_size, seed=0):
-    rnd = random.Random(seed)
-    image_base_dir = os.path.join("E:", "data", "wnet", "images")
-
-    images_per_file = 100
-    yield (len(gids) // batch_size) * images_per_file
-
-    while True:
-        rnd.shuffle(gids)
-        images = []
-
-        for gid in gids:
-            image = cv2.imread(os.path.join(image_base_dir, f"{gid}.png"), cv2.IMREAD_COLOR)
-            images += split_to_tiles(image / 255, 224)
-
-            while len(images) > batch_size:
-                image_batch = images[:batch_size]
-                images = images[batch_size:]
-                yield np.array(image_batch), np.array(image_batch)
-
-
-def make_training_and_validation_generators(batch_size=1, validation_split=0.1):
-    gids = get_gids_from_database("wnet")
-
-    rnd = random.Random(42)
-    rnd.shuffle(gids)
-
-    split = int(len(gids) * validation_split)
-
-    validation = gids[:split]
-    training = gids[split:]
-
-    return data_generator(training, batch_size, seed=17), data_generator(validation, batch_size, seed=29)
+from models.common.data_generator import initialize_train_and_validation_generators
+from models.wnet.wnet import WNet
 
 
 def predict(gids, model_path, num_classes=1000):
@@ -158,48 +66,38 @@ def restore(gids, model_path):
         cv2.imwrite(f"images/{gid}-restore.png", final_prediction)
 
 
-def do_training(start_time):
-    training_gen, validation_gen = make_training_and_validation_generators()
+def do_training():
+    gids = get_gids_from_database("wnet")
+    training_gen, validation_gen = initialize_train_and_validation_generators("wnet", gids, batch_size=10,
+                                                                              label_target_size=256)
     steps_per_epoch = next(training_gen)
     validation_steps = next(validation_gen)
 
-    if os.path.exists(f"weights/{start_time}"):
-        files = glob.glob(f"weights/{start_time}/*.hdf5")
-        run = len(files)
-        f = max(files, key=os.path.getctime)
-        f = "weights/1593163475/run-00__epoch-38__val-loss-0.94.hdf5"
+    full_model, encoder_model = WNet()
+    metrics = [Accuracy(), CategoricalAccuracy(), CategoricalCrossentropy()]
+    optimizer = None
+    full_model.compile(optimizer=optimizer, loss=MSE(), metrics=metrics)
 
-        dependencies = {
-        }
+    start_time = int(time.time())
+    model_path = f"weights/{start_time}_{full_model.name}/"
+    os.mkdir(model_path)
 
-        # TODO: load both models
-        model = load_model(f, custom_objects=dependencies)
-        set_value(model.optimizer.lr, 1e-5)
-        model.summary()
-    else:
-        run = 0
-        os.mkdir(f"weights/{start_time}")
-
-        metrics = [Accuracy(), CategoricalAccuracy()]
-        model, encoder = define_and_compile_model(metrics=metrics)
-        model.summary()
-        encoder.summary()
-
+    metrics_to_log = [metric.name for metric in metrics]
     callbacks = [
-        save_model_on_epoch_end("encoder-decoder", model, f"weights/{start_time}"),
-        save_model_on_epoch_end("encoder", encoder, f"weights/{start_time}"),
-        metrics_to_csv_logger(f"weights/{start_time}.csv", ["loss", "accuracy", "categorical_accuracy"]),
+        save_model_on_epoch_end(full_model.name, full_model, model_path),
+        save_model_on_epoch_end(encoder_model.name, encoder_model, model_path),
+        metrics_to_csv_logger(model_path + "/batch.csv", ["loss"] + metrics_to_log),
+        CSVLogger(model_path + "/epoch.csv", separator=";"),
+        ReduceLROnPlateau(monitor='loss', factor=0.1, patience=3, verbose=1, min_delta=1e-4, min_lr=1e-6),
     ]
-
-    model.fit(training_gen, epochs=50, steps_per_epoch=steps_per_epoch,
-              validation_data=validation_gen, validation_steps=validation_steps,
-              callbacks=callbacks)
+    full_model.fit(training_gen, epochs=50, steps_per_epoch=steps_per_epoch,
+                   validation_data=validation_gen, validation_steps=validation_steps,
+                   callbacks=callbacks)
 
 
 if __name__ == '__main__':
-    predict([51], "weights/1595520768/epoch_0_encoder_model.hdf5")
-    restore([51], "weights/1595520768/run-00__epoch-05__val-loss-1.48.hdf5")
-    exit(0)
+    # predict([51], "weights/1595520768/epoch_0_encoder_model.hdf5")
+    # restore([51], "weights/1595520768/run-00__epoch-05__val-loss-1.48.hdf5")
+    # exit(0)
 
-    start_time = int(time.time())
-    do_training(start_time)
+    do_training()
