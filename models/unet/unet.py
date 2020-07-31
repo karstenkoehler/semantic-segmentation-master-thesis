37f39a5d85d2cbe8723745e250d6cd2e3cdd5a43
Karsten Köhler <karsten.koehler95@gmail..com>
2020-07-31 09:23:36 +0200

    unet: simplify calculation of number of conv layers

--- models/unet/unet.py
+++ models/unet/unet.py
@@ -4,8 +4,6 @@ from tensorflow.keras.models import Model
 
 def UNet(feature_maps=None, nb_classes=6, dropout=0.5, model_name_suffix="", output_layer_activation="softmax",
          input_layer=None, input_size=None, conv_padding="valid"):
-    nb_conv_layers = 0
-
     if feature_maps is None:
         feature_maps = [64, 128, 256, 512, 1024]
     skip_connections = []
@@ -20,12 +18,10 @@ def UNet(feature_maps=None, nb_classes=6, dropout=0.5, model_name_suffix="", out
     for nb_filters in feature_maps[:-1]:
         x, skip = _downsampling_block(x, nb_filters, conv_padding)
         skip_connections += [skip]
-        nb_conv_layers += 2
 
     skip_connections = skip_connections[::-1]
     x = Conv2D(feature_maps[-1], 3, activation='relu', padding=conv_padding, kernel_initializer='he_normal')(x)
     x = Conv2D(feature_maps[-1], 3, activation='relu', padding=conv_padding, kernel_initializer='he_normal')(x)
-    nb_conv_layers += 2
 
     if dropout > 0.0:
         x = Dropout(dropout)(x)
@@ -33,11 +29,10 @@ def UNet(feature_maps=None, nb_classes=6, dropout=0.5, model_name_suffix="", out
     for i, nb_filters in enumerate(feature_maps[:-1][::-1]):
         crop_px = (skip_connections[i].shape[1] - (x.shape[1] * 2)) // 2
         x = _upsampling_block(x, skip_connections[i], nb_filters, crop_px, conv_padding)
-        nb_conv_layers += 3
 
     output_layer = Conv2D(nb_classes, 1, activation=output_layer_activation, padding=conv_padding)(x)
-    nb_conv_layers += 1
 
+    nb_conv_layers = 3 + 5 * (len(feature_maps) - 1)
     model_name = f"unet-{nb_conv_layers}{'D' if dropout > 0.0 else ''}{model_name_suffix}"
     return Model(inputs=input_layer, outputs=output_layer, name=model_name), (input_layer, output_layer)
 
