930bf4d10240725f0614d70e5e343ecb6a84f69b
Karsten Koehler <karsten.koehler95@gmail.com>
2020-09-20 09:42:01 +0200

    wnet: update hyperparameters

--- models/wnet/model.py
+++ models/wnet/model.py
@@ -53,16 +53,16 @@ def restore(gids, model_path, mode="train"):
 
 def do_training(initial_learning_rate=0.001):
     gids = get_gids_from_database("wnet")
-    training_gen, validation_gen = initialize_train_and_validation_generators("wnet", gids, batch_size=10,
+    training_gen, validation_gen = initialize_train_and_validation_generators("wnet", gids, batch_size=5,
                                                                               label_target_size=256,
                                                                               use_image_as_label=True)
     steps_per_epoch = next(training_gen)
     validation_steps = next(validation_gen)
 
-    full_model, encoder_model = WNet()
+    full_model, encoder_model = WNet(nb_classes=6)
     metrics = [Accuracy(), CategoricalAccuracy(), CategoricalCrossentropy()]
     optimizer = Adam(lr=initial_learning_rate)
-    full_model.compile(optimizer=optimizer, loss=MSE, metrics=metrics)
+    full_model.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=metrics)
 
     start_time = int(time.time())
     model_path = f"weights/{start_time}_{full_model.name}/"
@@ -76,14 +76,15 @@ def do_training(initial_learning_rate=0.001):
         CSVLogger(model_path + "/epoch.csv", separator=";"),
         ReduceLROnPlateau(monitor='loss', factor=0.1, patience=3, verbose=1, min_delta=1e-4, min_lr=1e-6),
     ]
-    full_model.fit(training_gen, epochs=50, steps_per_epoch=steps_per_epoch,
+    full_model.fit(training_gen, epochs=10, steps_per_epoch=steps_per_epoch,
                    validation_data=validation_gen, validation_steps=validation_steps,
                    callbacks=callbacks)
 
 
 if __name__ == '__main__':
-    # predict([431], "weights/1596694200_WNet-46D-6/WNet-46D-6-Encoder_epoch_20.hdf5")
-    # restore([431], "weights/1596694200_WNet-46D-6/WNet-46D-6_epoch_20.hdf5")
+    # gids = [63585, 224817, 19743, 78905, 102574, 119756]
+    # predict(gids, "weights/1596694200_WNet-46D-6/WNet-46D-6-Encoder_epoch_9.hdf5", mode="test")
+    # restore(gids, "weights/1596694200_WNet-46D-6/WNet-46D-6_epoch_9.hdf5", mode="test")
     # exit(0)
 
     do_training()
