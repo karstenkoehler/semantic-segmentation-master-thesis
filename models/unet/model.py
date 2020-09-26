0f19a2f8144bd5b49091a7c17866ca5be7911180
Karsten Koehler <karsten.koehler95@gmail.com>
2020-09-26 16:14:58 +0200

    unet: clean up

--- models/unet/model.py
+++ models/unet/model.py
@@ -1,18 +1,21 @@
 import os
 import time
+import random
 
 import cv2
 import numpy as np
+import tensorflow as tf
+
 from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.metrics import Accuracy, CategoricalAccuracy, CategoricalCrossentropy
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import SGD
-from tensorflow.python.keras.callbacks import LearningRateScheduler, CSVLogger
+from tensorflow.keras.callbacks import LearningRateScheduler, CSVLogger
 
 from models.common.callbacks import metrics_to_csv_logger, save_model_on_epoch_end
 from models.common.common import get_gids_from_database, one_hot_to_rgb, one_hot_encoding
 from models.common.data_generator import initialize_train_and_validation_generators
-from models.common.metrics import ArgmaxMeanIoU, TF_CUSTOM_METRICS, weighted_categorical_crossentropy
+from models.common.metrics import ArgmaxMeanIoU, TF_CUSTOM_METRICS
 from models.unet.unet import UNet
 
 
@@ -57,20 +60,11 @@ def do_training(initial_learning_rate=0.1):
     steps_per_epoch = next(training_gen)
     validation_steps = next(validation_gen)
 
-    class_weights = [
-        0.0813,  # buildings
-        0.5272,  # water
-        0.0482,  # forest
-        1.0000,  # traffic
-        0.4133,  # urban greens
-        0.0480,  # agriculture
-    ]
-
-    model = UNet(input_size=(572, 572, 3), model_name_suffix="-CW")
+    model = UNet(input_size=(572, 572, 3))
     metrics = [Accuracy(), CategoricalAccuracy(),
                CategoricalCrossentropy(), ArgmaxMeanIoU(num_classes=6, name="mean_iou")]
     optimizer = SGD(learning_rate=initial_learning_rate, momentum=0.99, nesterov=True)
-    model.compile(optimizer=optimizer, loss=weighted_categorical_crossentropy(class_weights), metrics=metrics)
+    model.compile(optimizer=optimizer, loss=categorical_crossentropy, metrics=metrics)
 
     start_time = int(time.time())
     os.mkdir(f"weights/{start_time}_{model.name}/")
@@ -90,8 +84,13 @@ def do_training(initial_learning_rate=0.1):
 
 
 if __name__ == '__main__':
-    # predict([218728, 165639, 115316, 27516], "weights/1595707740_unet-23D/unet-23D_epoch_20.hdf5")
-    # predict([104483, 146915, 160918], "weights/1595707740_unet-23D/unet-23D_epoch_20.hdf5", mode="test")
+    np.random.seed(1595840929)
+    random.seed(1595840929)
+    tf.random.set_seed(1595840929)
+
+    # model_path = "weights/1597170026_unet-28D/unet-28D_epoch_4.hdf5"
+    # predict([218728, 165639, 115316, 27516], model_path)
+    # predict([104483, 147352, 82607, 114975, 138625, 35010, 85227, 117569, 85228, 66861], model_path, mode="test")
     # exit(0)
 
     do_training()
